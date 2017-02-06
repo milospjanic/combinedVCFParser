d5ec4fe602d3cf6814115286943a609bd765135a
Milos Pjanic <pjanic.milos@gmail.com>
2017-02-05 19:01:28 -0800

    Update combinedVCFParser.sh

--- combinedVCFParser.sh
+++ combinedVCFParser.sh
@@ -3,7 +3,7 @@ cat >> script.awk <<EOL
 
 #find a line with CHROM in the vcf file, go through the fields, place in hash h1, key=position, value=field content
 
-/#[CHROM|CHR|chrom|chr]/ { for(i = 1; i <= NF; i++) {h1[i] = \$i}}
+/#(CHROM|CHR|chrom|chr)/ { for(i = 1; i <= NF; i++) {h1[i] = \$i}}
 
 #find a line with SNP ID, go through the fields, if field contains 0/1 or 0|1 print het, hash value (sample name), position, content 
 
